984b46f8f3a27d3dfd24d23daa328207879154fe
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-01-20 13:34:53 +0100

    Add foreign keys signature

--- movie-app-server/init-db.js
+++ movie-app-server/init-db.js
@@ -5,9 +5,9 @@ const db = new sqlite3.Database(dbPath);
 db.serialize(() => {
   db.run('CREATE TABLE IF NOT EXISTS movies (id_movie INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, release TEXT NOT NULL, img TEXT approved INTEGER DEFAULT 0)');
   db.run('CREATE TABLE IF NOT EXISTS users (id_user INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL, password TEXT NOT NULL, img TEXT)');
-  db.run('CREATE TABLE IF NOT EXISTS rates (id_rate INTEGER PRIMARY KEY AUTOINCREMENT, movie_id INTEGER, user_id INTEGER, r_p INTEGER, r_ac INTEGER, r_s INTEGER, r_au INTEGER, r_all INTEGER)');
+  db.run('CREATE TABLE IF NOT EXISTS rates (id_rate INTEGER PRIMARY KEY AUTOINCREMENT, movie_id INTEGER, user_id INTEGER, r_p INTEGER, r_ac INTEGER, r_s INTEGER, r_au INTEGER, r_all INTEGER, FOREIGN KEY(movie_id) REFERENCES movies(id_movie), FOREIGN KEY(user_id) REFERENCES users(id_user))');
   db.run('CREATE TABLE IF NOT EXISTS persons (id_person INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, surname TEXT, birth_date TEXT)');
-  db.run('CREATE TABLE IF NOT EXISTS creators (id_creator INTEGER PRIMARY KEY AUTOINCREMENT, movie_id INTEGER, person_id INTEGER, role INTEGER)');
+  db.run('CREATE TABLE IF NOT EXISTS creators (id_creator INTEGER PRIMARY KEY AUTOINCREMENT, movie_id INTEGER, person_id INTEGER, role INTEGER, FOREIGN KEY(movie_id) REFERENCES movies(id_movie), FOREIGN KEY(person_id) REFERENCES persons(person_id))');
 
 console.log('Created database');
 });
