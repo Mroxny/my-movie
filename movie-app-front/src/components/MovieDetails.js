84f497961a59101a995719b28e75cee210d4aa92
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-01-21 22:48:17 +0100

    Update MovieDetails.js

--- movie-app-front/src/components/MovieDetails.js
+++ movie-app-front/src/components/MovieDetails.js
@@ -13,26 +13,39 @@ const MovieDetails = ({ movie }) => {
       .catch(error => console.error('Error getting creators:', error));
   }, [movie]);
 
-
   console.log("Creators: "+creators)
+
+
   return (
     <div className="details-page">
       <img className='details-page-image' src={movie.img} alt={movie.title} />
       <div>
         <h2>{movie.title}</h2>
         <p>Rok produkcji: {movie.release}</p>
-        <p><b>Ogólne oceny: {movie.avg_r_all.toFixed(1)}</b></p>
-        <p>Oceny zdjęć: {movie.avg_r_p.toFixed(1)}</p>
-        <p>Oceny aktorów: {movie.avg_r_ac.toFixed(1)}</p>
-        <p>Oceny scenariusza: {movie.avg_r_s.toFixed(1)}</p>
-        <p>Oceny dzwięku: {movie.avg_r_au.toFixed(1)}</p>
-        <Link to={`/rates/movie/${movie.id_movie}`} className=''>Zobacz oceny innych użytkowników</Link>
-          <h3>Zaangażowani w produkcję:</h3>
-          <ul>
-            {creators.map((creator, index) => (
-              <li key={index}>{`${creator.person_name} ${creator.person_surname} (${creator.person_birth_date}) - ${creator.role}`}</li>
-            ))}
-          </ul>
+        {movie.avg_r_all && 
+        (
+          <div>
+            <p><b>Ogólne oceny: {movie.avg_r_all.toFixed(1)}</b></p>
+            <p>Oceny zdjęć: {movie.avg_r_p.toFixed(1)}</p>
+            <p>Oceny aktorów: {movie.avg_r_ac.toFixed(1)}</p>
+            <p>Oceny scenariusza: {movie.avg_r_s.toFixed(1)}</p>
+            <p>Oceny dzwięku: {movie.avg_r_au.toFixed(1)}</p>
+            <Link to={`/rates/movie/${movie.id_movie}`} className=''>Zobacz oceny innych użytkowników</Link>
+          </div>
+        )}
+        {creators.length > 0 &&
+        (
+          <div>
+            <h3>Zaangażowani w produkcję:</h3>
+            <ul>
+              {creators.map((creator, index) => (
+                <li key={index}>{`${creator.person_name} ${creator.person_surname} (${creator.person_birth_date}) - ${creator.role}`}</li>
+              ))}
+            </ul>
+          </div>
+        )
+        }
+
 
       </div>
     </div>
