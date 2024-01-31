24efe16ecaa9ad91f9a82a0f371b836baa415506
Maciej Mrozek <Maciej.h.mrozek@gmail.com>
2024-01-31 13:51:13 +0100

    Update userControler

--- my-movie_server/init-db.js
+++ my-movie_server/init-db.js
@@ -2,6 +2,18 @@ const sqlite3 = require('sqlite3').verbose();
 const dbPath = './data.db';
 const db = new sqlite3.Database(dbPath);
 
+function isTableEmpty(tableName) {
+  return new Promise((resolve, reject) => {
+    db.get(`SELECT COUNT(*) as count FROM ${tableName}`, (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row.count === 0);
+      }
+    });
+  });
+}
+
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS users 
   (id_user INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -31,53 +43,101 @@ db.serialize(() => {
   (id_rate_type INTEGER PRIMARY KEY AUTOINCREMENT, 
     rate_name TEXT, 
     icon TEXT)`);
-
-  const initialRoomData = [
-    { room_name: "admin's room", img: "" },
-  ];
-
-  const initialUserData = [
-    { username: 'admin', password: "$2b$10$swD4Tbzlo0UkZFGJOYK7y.Ft72TYNW5Q7hnzB5ndgFL.7eMAogy/i", room_id: 1, isAdmin: 1 }
-  ];
-
-  const initialRateData = [
-    { user_id: 1, movie_id: 1, rate_type: 1, rate_value: 5 , rate_date: "2001-01-01 10:11:12" },
-    { user_id: 1, movie_id: 1, rate_type: 2, rate_value: 4 , rate_date: "2001-01-01 10:11:12" },
-    { user_id: 1, movie_id: 1, rate_type: 3, rate_value: 5 , rate_date: "2001-01-01 10:11:12" }
-  ];
-
-  const initialRateTypeData = [
-    { rate_name: 'main', icon: "" },
-    { rate_name: 'pictures', icon: "" },
-    { rate_name: 'acting', icon: "" }
-  ];
-
-  const insertRoom = db.prepare('INSERT INTO rooms (room_name, img) VALUES (?, ?)');
-  const insertUser = db.prepare('INSERT INTO users (username, password, room_id, isAdmin) VALUES (?, ?, ?, ?)');
-  const insertRate = db.prepare('INSERT INTO rates (user_id, movie_id, rate_type, rate_value, rate_value) VALUES (?, ?, ?, ?, ?)');
-  const insertRateType = db.prepare('INSERT INTO rate_types (rate_name, icon) VALUES (?, ?)');
-
-  initialRoomData.forEach((room) => {
-    insertRoom.run(room.room_name, room.img);
-  });
-  insertRoom.finalize();
-
-  initialUserData.forEach((user) => {
-    insertUser.run(user.username, user.password, user.isAdmin);
-  });
-  insertUser.finalize();
-
-  initialRateData.forEach((rate) => {
-    insertRate.run(rate.user_id, rate.movie_id, rate.rate_type, rate.rate_value, rate.rate_date);
-  });
-  insertRate.finalize();
-
-  initialRateTypeData.forEach((rate_type) => {
-    insertRateType.run(rate_type.rate_name, rate_type.icon);
-  });
-  insertRateType.finalize();
-
-  console.log('Created database');
 });
 
-db.close();
+isTableEmpty('users')
+  .then((isEmpty) => {
+    if (isEmpty) {
+      // Insert initial user data
+      const initialUserData = [
+        { username: 'admin', password: "$2b$10$swD4Tbzlo0UkZFGJOYK7y.Ft72TYNW5Q7hnzB5ndgFL.7eMAogy/i", room_id: 1, isAdmin: 1 }
+      ];
+      const insertUser = db.prepare('INSERT INTO users (username, password, room_id, isAdmin) VALUES (?, ?, ?, ?)');
+
+      initialUserData.forEach((user) => {
+        insertUser.run(user.username, user.password, user.room_id, user.isAdmin);
+      });
+
+      insertUser.finalize();
+      console.log('Inserted initial user data');
+    }
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+    db.close();
+  });
+
+isTableEmpty('rooms')
+  .then((isEmpty) => {
+    if (isEmpty) {
+      // Insert initial room data
+      const initialRoomData = [
+        { room_name: "admin's room", img: "" },
+      ];
+      const insertRoom = db.prepare('INSERT INTO rooms (room_name, img) VALUES (?, ?)');
+
+      initialRoomData.forEach((room) => {
+        insertRoom.run(room.room_name, room.img);
+      });
+
+      insertRoom.finalize();
+      console.log('Inserted initial room data');
+    }
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+    db.close();
+  });
+
+isTableEmpty('rates')
+  .then((isEmpty) => {
+    if (isEmpty) {
+      // Insert initial rate data
+      const initialRateData = [
+        { user_id: 1, movie_id: 1, rate_type: 1, rate_value: 5 , rate_date: "2001-01-01 10:11:12" },
+        { user_id: 1, movie_id: 1, rate_type: 2, rate_value: 4 , rate_date: "2001-01-01 10:11:12" },
+        { user_id: 1, movie_id: 1, rate_type: 3, rate_value: 5 , rate_date: "2001-01-01 10:11:12" }
+      ];
+      const insertRate = db.prepare('INSERT INTO rates (user_id, movie_id, rate_type, rate_value, rate_date) VALUES (?, ?, ?, ?, ?)');
+
+      initialRateData.forEach((rate) => {
+        insertRate.run(rate.user_id, rate.movie_id, rate.rate_type, rate.rate_value, rate.rate_date);
+      });
+
+      insertRate.finalize();
+      console.log('Inserted initial rate data');
+    }
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+    db.close();
+  });
+
+isTableEmpty('rate_types')
+  .then((isEmpty) => {
+    if (isEmpty) {
+      // Insert initial rate type data
+      const initialRateTypeData = [
+        { rate_name: 'main', icon: "" },
+        { rate_name: 'pictures', icon: "" },
+        { rate_name: 'acting', icon: "" }
+      ];
+      const insertRateType = db.prepare('INSERT INTO rate_types (rate_name, icon) VALUES (?, ?)');
+
+      initialRateTypeData.forEach((rate_type) => {
+        insertRateType.run(rate_type.rate_name, rate_type.icon);
+      });
+
+      insertRateType.finalize();
+      console.log('Inserted initial rate type data');
+    }
+  })
+  .then(() => {
+    console.log('Database setup complete');
+    db.close();
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+    db.close();
+  });
+
--- my-movie_server/routes/index.js
+++ my-movie_server/routes/index.js
@@ -14,7 +14,7 @@ const verifyToken = (req, res, next) => {
   const token = req.header('Authorization');
 
   if (!token) {
-    return res.status(401).json({ error: 'Auth failed, access denied' });
+    return res.status(401).json({ error: 'Authorization failed, access denied' });
   }
 
   const JWT_SECRET = process.env.JWT_SECRET;
@@ -24,7 +24,7 @@ const verifyToken = (req, res, next) => {
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(401).json({ error: 'Błąd weryfikacji tokenu, autoryzacja nieudana' });
+    res.status(401).json({ error: 'Token verification error, authorization failed' });
   }
 };
 
@@ -36,36 +36,28 @@ router.use((req, res, next) => {
 });
 
 // movies
-router.get('/movies', MovieController.getAllMovies);
-router.get('/movies/:id', MovieController.getMovieById);
-router.post('/movies', MovieController.addMovie);
-router.put('/movies/:id', MovieController.updateMovie);
-router.delete('/movies/:id', MovieController.deleteMovie);
+// router.get('/movies', MovieController.getAllMovies);
+// router.get('/movies/:id', MovieController.getMovieById);
+// router.post('/movies', MovieController.addMovie);
+// router.put('/movies/:id', MovieController.updateMovie);
+// router.delete('/movies/:id', MovieController.deleteMovie);
 
 // users
 router.get('/login', UserController.getUserToken);
 router.get('/users', verifyToken, UserController.getAllUsers);
 router.get('/users/:id', verifyToken, UserController.getUserById);
-router.get('/users/email/:email', verifyToken, UserController.getUserByEmail);
+router.get('/users/username/:username', verifyToken, UserController.getUserByUsername);
 router.post('/users', UserController.addUser);
 router.put('/users/:id', verifyToken, UserController.updateUser);
 router.delete('/users/:id', verifyToken, UserController.deleteUser);
 
 // rates
-router.get('/rates', RateController.getAllRates);
-router.get('/rates/:id', RateController.getRateById);
-router.get('/rates/user/:idUser', RateController.getRatesByUser);
-router.get('/rates/movie/:idMovie', RateController.getRatesByMovie);
-router.post('/rates', RateController.addRate);
-router.put('/rates/:id', RateController.updateRate);
-router.delete('/rates/:id', RateController.deleteRate);
-
-// creators
-router.get('/creators', CreatorController.getAllCreators);
-router.get('/creators/:id', CreatorController.getCreatorById);
-router.get('/creators/movie/:idMovie', CreatorController.getCreatorsByMovie);
-router.post('/creators', CreatorController.addCreator);
-router.put('/creators/:id', CreatorController.updateCreator);
-router.delete('/creators/:id', CreatorController.deleteCreator);
+// router.get('/rates', RateController.getAllRates);
+// router.get('/rates/:id', RateController.getRateById);
+// router.get('/rates/user/:idUser', RateController.getRatesByUser);
+// router.get('/rates/movie/:idMovie', RateController.getRatesByMovie);
+// router.post('/rates', RateController.addRate);
+// router.put('/rates/:id', RateController.updateRate);
+// router.delete('/rates/:id', RateController.deleteRate);
 
 module.exports = router;
