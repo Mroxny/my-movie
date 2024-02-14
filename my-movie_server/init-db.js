b9c1161eaf687e0e2a62069cf6d57b45cec46ce6
Maciej Mrozek <Maciej.h.mrozek@gmail.com>
2024-02-14 13:19:30 +0100

    Change database structure

--- my-movie_server/init-db.js
+++ my-movie_server/init-db.js
@@ -17,12 +17,13 @@ function isTableEmpty(tableName) {
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS users 
   (id_user INTEGER PRIMARY KEY AUTOINCREMENT, 
+    room_id INTEGER,
     username TEXT NOT NULL, 
     password TEXT NOT NULL, 
     email TEXT, 
     img TEXT, 
-    room_id INTEGER,
-    isAdmin INTEGER DEFAULT 0)`);
+    isAdmin INTEGER DEFAULT 0,
+    FOREIGN KEY(room_id) REFERENCES rooms(id_room))`);
 
   db.run(`CREATE TABLE IF NOT EXISTS rooms 
   (id_room INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -32,30 +33,37 @@ db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS rates 
   (id_rate INTEGER PRIMARY KEY AUTOINCREMENT, 
     user_id INTEGER, 
-    movie_id INTEGER, 
-    rate_type INTEGER, 
+    entity_type INTEGER, 
+    entity_id INTEGER,
     rate_value INTEGER,
     rate_date TEXT, 
-    FOREIGN KEY(rate_type) REFERENCES rate_types(id_rate_type), 
     FOREIGN KEY(user_id) REFERENCES users(id_user))`);
 
-  db.run(`CREATE TABLE IF NOT EXISTS rate_types 
-  (id_rate_type INTEGER PRIMARY KEY AUTOINCREMENT, 
-    rate_name TEXT, 
-    icon TEXT)`);
+  db.run(`CREATE TABLE IF NOT EXISTS lists 
+  (id_list INTEGER PRIMARY KEY AUTOINCREMENT, 
+    room_id INTEGER, 
+    name TEXT,
+    FOREIGN KEY(room_id) REFERENCES rooms(id_room))`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS entityInList 
+  (id_entity_in_list INTEGER PRIMARY KEY AUTOINCREMENT, 
+    list_id INTEGER, 
+    entity_type INTEGER, 
+    entity_id INTEGER,
+    FOREIGN KEY(list_id) REFERENCES lists(id_list))`);
 });
 
+// Insert initial user data
 isTableEmpty('users')
   .then((isEmpty) => {
     if (isEmpty) {
-      // Insert initial user data
       const initialUserData = [
         { username: 'admin', password: "$2b$10$swD4Tbzlo0UkZFGJOYK7y.Ft72TYNW5Q7hnzB5ndgFL.7eMAogy/i", room_id: 1, isAdmin: 1 }
       ];
-      const insertUser = db.prepare('INSERT INTO users (username, password, room_id, isAdmin) VALUES (?, ?, ?, ?)');
+      const insertUser = db.prepare('INSERT INTO users (room_id, username, password, isAdmin) VALUES (?, ?, ?, ?)');
 
       initialUserData.forEach((user) => {
-        insertUser.run(user.username, user.password, user.room_id, user.isAdmin);
+        insertUser.run(user.room_id, user.username, user.password, user.isAdmin);
       });
 
       insertUser.finalize();
@@ -67,10 +75,10 @@ isTableEmpty('users')
     db.close();
   });
 
+// Insert initial room data
 isTableEmpty('rooms')
   .then((isEmpty) => {
     if (isEmpty) {
-      // Insert initial room data
       const initialRoomData = [
         { room_name: "admin's room", img: "" },
       ];
@@ -89,19 +97,19 @@ isTableEmpty('rooms')
     db.close();
   });
 
+// Insert initial rate data
 isTableEmpty('rates')
   .then((isEmpty) => {
     if (isEmpty) {
-      // Insert initial rate data
       const initialRateData = [
-        { user_id: 1, movie_id: 1, rate_type: 1, rate_value: 5 , rate_date: "2001-01-01 10:11:12" },
-        { user_id: 1, movie_id: 1, rate_type: 2, rate_value: 4 , rate_date: "2001-01-01 10:11:12" },
-        { user_id: 1, movie_id: 1, rate_type: 3, rate_value: 5 , rate_date: "2001-01-01 10:11:12" }
+        { user_id: 1, entity_type: 0, entity_id: 1, rate_value: 5, rate_date: "2001-01-01 10:11:12" },
+        { user_id: 1, entity_type: 0, entity_id: 2, rate_value: 4, rate_date: "2001-01-01 10:12:12" },
+        { user_id: 1, entity_type: 0, entity_id: 3, rate_value: 5, rate_date: "2001-01-01 10:13:12" }
       ];
-      const insertRate = db.prepare('INSERT INTO rates (user_id, movie_id, rate_type, rate_value, rate_date) VALUES (?, ?, ?, ?, ?)');
+      const insertRate = db.prepare('INSERT INTO rates (user_id, entity_type, entity_id, rate_value, rate_date) VALUES (?, ?, ?, ?, ?)');
 
       initialRateData.forEach((rate) => {
-        insertRate.run(rate.user_id, rate.movie_id, rate.rate_type, rate.rate_value, rate.rate_date);
+        insertRate.run(rate.user_id, rate.entity_type, rate.entity_id, rate.rate_value, rate.rate_date);
       });
 
       insertRate.finalize();
@@ -113,23 +121,45 @@ isTableEmpty('rates')
     db.close();
   });
 
-isTableEmpty('rate_types')
+// Insert initial lists data
+isTableEmpty('lists')
   .then((isEmpty) => {
     if (isEmpty) {
-      // Insert initial rate type data
-      const initialRateTypeData = [
-        { rate_name: 'main', icon: "" },
-        { rate_name: 'pictures', icon: "" },
-        { rate_name: 'acting', icon: "" }
+      const initialListData = [
+        { room_id: 1, name: "Favourites" }
       ];
-      const insertRateType = db.prepare('INSERT INTO rate_types (rate_name, icon) VALUES (?, ?)');
+      const insertListType = db.prepare('INSERT INTO lists (room_id, name) VALUES (?, ?)');
 
-      initialRateTypeData.forEach((rate_type) => {
-        insertRateType.run(rate_type.rate_name, rate_type.icon);
+      initialListData.forEach((list) => {
+        insertListType.run(list.room_id, list.name);
       });
 
-      insertRateType.finalize();
-      console.log('Inserted initial rate type data');
+      insertListType.finalize();
+      console.log('Inserted initial lists data');
+    }
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+    db.close();
+  });
+
+// Insert initial entity in list data
+isTableEmpty('entityInList')
+  .then((isEmpty) => {
+    if (isEmpty) {
+      const initialEntityInListData = [
+        { list_id: 1, entity_type: 0, entity_id: 1 },
+        { list_id: 1, entity_type: 0, entity_id: 2 },
+        { list_id: 1, entity_type: 0, entity_id: 3 }
+      ];
+      const insertEntityInList = db.prepare('INSERT INTO entityInList (list_id, entity_type, entity_id) VALUES (?, ?, ?)');
+
+      initialEntityInListData.forEach((e) => {
+        insertEntityInList.run(e.list_id, e.entity_type, e.entity_id);
+      });
+
+      insertEntityInList.finalize();
+      console.log('Inserted initial entity in list data');
     }
   })
   .then(() => {
