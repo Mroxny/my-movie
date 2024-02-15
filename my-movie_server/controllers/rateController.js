67ebd39c227da64ce852f39ec45df6aa99a27f5a
Mroxny <50219282+Mroxny@users.noreply.github.com>
2024-02-15 13:43:17 +0100

    Fix rate tests

--- my-movie_server/controllers/rateController.js
+++ my-movie_server/controllers/rateController.js
@@ -1,153 +1,160 @@
-const Rate = require('../models/rateModel');
-
+const Rate = require("../models/rateModel");
 
 class RateController {
-  static getAllRates(req, res) {
-    Rate.getAll((err, result) => {
-      if (err) {
-        res.status(500).json({ error: 'Server error' });
-      } else {
-        res.json(result);
-      }
-    });
-  }
-
-  static getRateById(req, res) {
-    const idRate = req.params.id;
-
-    Rate.getById(idRate, (err, result) => {
-      if (err) {
-        res.status(500).json({ error: 'Server error' });
-      } else {
-        res.json(result[0]);
-      }
-    });
-  }
-
-  static getRatesByMovie(req, res) {
-    const idMovie = req.params.idMovie;
-
-    Rate.getByMovie(idMovie, (err, result) => {
-      if (err) {
-        res.status(500).json({ error: 'Server error' });
-      } else {
-        res.json(result);
-      }
-    });
-  }
-
-
-  static getRatesByUser(req, res) {
-    const idUser = req.params.idUser;
-
-    Rate.getByUser(idUser, (err, result) => {
-      if (err) {
-        res.status(500).json({ error: 'Server error' });
-      } else {
-        res.json(result);
-      }
-    });
-  }
-
-  static getRatesCountByUser(req, res) {
-    const idUser = req.params.idUser;
-
-    Rate.getCountByUser(idUser, (err, result) => {
-      if (err) {
-        res.status(500).json({ error: 'Server error' });
-      } else {
-        res.json(result[0]);
-      }
-    });
-  }
-
-  static addRate(req, res) {
-    const {user_id, entity_type, entity_id, rate_value, rate_date} = req.body;
-    console.log(req.body)
-    if (!user_id || !entity_type || !entity_id || !rate_value || !rate_date) {
-      res.status(400).json({ error: 'Invalid input data' });
-      return;
-    }
-
-    const dateFormatRegex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
-    if (!dateFormatRegex.test(rate_date)) {
-        res.status(400).json({ error: 'Invalid date format. Please provide the date in the format: YYYY-MM-DD HH:mm:ss' });
-        return;
-    }
-
-    const rateData = {user_id, entity_type, entity_id, rate_value, rate_date};
-
-
-    Rate.addRate(rateData, (err, result) => {
-      if (err) {
-        res.status(500).json({ error: 'Server error' });
-      } else {
-        res.status(201).json(result);
-      }
-    });
-  }
-
-  static updateRate(req, res) {
-    const idRate = req.params.id;
-    const {user_id, entity_type, entity_id, rate_value, rate_date} = req.body;
-
-    if (!entity_type && !user_id && !entity_id && !rate_value && !rate_date) {
-      res.status(400).json({ error: 'Invalid input data' });
-      return;
-    }
-
-    Rate.getById(idRate, (err, rate) => {
-      if (err) {
-        res.status(500).json({ error: 'Server error' });
-      } else {
-        if (!rate) {
-          res.status(404).json({ error: 'Rate not found' });
-        } else {
-          const updatedData = {}
-          
-          if (user_id !== undefined) {
-            updatedData.user_id = user_id;
-          }
-
-          if (entity_type !== undefined) {
-            updatedData.entity_type = entity_type;
-          }
-
-          if (entity_id !== undefined) {
-            updatedData.entity_id = entity_id;
-          }
-
-          if (rate_value !== undefined) {
-            updatedData.rate_value = rate_value;
-          }
-
-          if (rate_date !== undefined) {
-            updatedData.rate_date = rate_date;
-          }
-
-          Rate.updateRate(idRate, updatedData, (err, result) => {
+    static getAllRates(req, res) {
+        Rate.getAll((err, result) => {
             if (err) {
-              res.status(500).json({ error: 'Server error' });
+                res.status(500).json({ error: "Server error" });
             } else {
-              res.json(result);
+                res.json(result);
             }
-          });
+        });
+    }
+
+    static getRateById(req, res) {
+        const idRate = req.params.id;
+
+        Rate.getById(idRate, (err, result) => {
+            if (err) {
+                res.status(500).json({ error: "Server error" });
+            } else {
+                res.json(result[0]);
+            }
+        });
+    }
+
+    static getRatesByMovie(req, res) {
+        const idMovie = req.params.idMovie;
+
+        Rate.getByMovie(idMovie, (err, result) => {
+            if (err) {
+                res.status(500).json({ error: "Server error" });
+            } else {
+                res.json(result);
+            }
+        });
+    }
+
+    static getRatesByUser(req, res) {
+        const idUser = req.params.idUser;
+
+        Rate.getByUser(idUser, (err, result) => {
+            if (err) {
+                res.status(500).json({ error: "Server error" });
+            } else {
+                res.json(result);
+            }
+        });
+    }
+
+    static getRatesCountByUser(req, res) {
+        const idUser = req.params.idUser;
+
+        Rate.getCountByUser(idUser, (err, result) => {
+            if (err) {
+                res.status(500).json({ error: "Server error" });
+            } else {
+                res.json(result[0]);
+            }
+        });
+    }
+
+    static addRate(req, res) {
+        const { user_id, entity_type, entity_id, rate_value, rate_date } = req.body;
+        console.log("Adding rate body:" + JSON.stringify(req.body));
+
+        if (!user_id || !entity_type || !entity_id || !rate_value || !rate_date) {
+            res.status(400).json({ error: "Invalid input data" });
+            return;
         }
-      }
-    });
-  }
 
-  static deleteRate(req, res) {
-    const idRate = req.params.id;
+        const dateFormatRegex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
+        if (!dateFormatRegex.test(rate_date)) {
+            res.status(400).json({
+                error: "Invalid date format. Please provide the date in the format: YYYY-MM-DD HH:mm:ss",
+            });
+            return;
+        }
 
-    Rate.deleteRate(idRate, (err, result) => {
-      if (err) {
-        res.status(500).json({ error: 'Server error' });
-      } else {
-        res.json(result);
-      }
-    });
-  }
+        const rateData = {
+            user_id,
+            entity_type,
+            entity_id,
+            rate_value,
+            rate_date,
+        };
+
+        Rate.addRate(rateData, (err, result) => {
+            if (err) {
+                res.status(500).json({ error: "Server error" });
+            } else {
+                res.status(201).json(result);
+            }
+        });
+    }
+
+    static updateRate(req, res) {
+        const idRate = req.params.id;
+        const { user_id, entity_type, entity_id, rate_value, rate_date } = req.body;
+        console.log("Updating rate body:" + JSON.stringify(req.body));
+
+        if (!entity_type && !user_id && !entity_id && !rate_value && !rate_date) {
+            res.status(400).json({ error: "Invalid input data" });
+            return;
+        }
+
+        Rate.getById(idRate, (err, rate) => {
+            if (err) {
+                res.status(500).json({ error: "Server error" });
+            } else {
+                if (!rate) {
+                    res.status(404).json({ error: "Rate not found" });
+                } else {
+                    const updatedData = {};
+
+                    if (user_id !== undefined) {
+                        updatedData.user_id = user_id;
+                    }
+
+                    if (entity_type !== undefined) {
+                        updatedData.entity_type = entity_type;
+                    }
+
+                    if (entity_id !== undefined) {
+                        updatedData.entity_id = entity_id;
+                    }
+
+                    if (rate_value !== undefined) {
+                        updatedData.rate_value = rate_value;
+                    }
+
+                    if (rate_date !== undefined) {
+                        updatedData.rate_date = rate_date;
+                    }
+
+                    Rate.updateRate(idRate, updatedData, (err, result) => {
+                        if (err) {
+                            res.status(500).json({ error: "Server error" });
+                        } else {
+                            res.json(result);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    static deleteRate(req, res) {
+        const idRate = req.params.id;
+
+        Rate.deleteRate(idRate, (err, result) => {
+            if (err) {
+                res.status(500).json({ error: "Server error" });
+            } else {
+                res.json(result);
+            }
+        });
+    }
 }
 
 module.exports = RateController;
--- my-movie_server/tests/data/rateEndpoints.test.data.js
+++ my-movie_server/tests/data/rateEndpoints.test.data.js
@@ -1,25 +1,25 @@
 const reqAddRate = {
-    user_id: 1, 
-    entity_type: 0,
+    user_id: 1,
+    entity_type: 1,
     entity_id: 1,
     rate_value: 5,
-    rate_date: "2022-12-12 15:10:05"
-}
+    rate_date: "2022-12-12 15:10:05",
+};
 
 const reqAddInvalidRate = {
-    user_id: 1, 
-    entity_type: 0,
+    user_id: 1,
+    entity_type: 1,
     entity_id: 1,
     rate_value: 5,
-    rate_date: "2022"
-}
+    rate_date: "2022",
+};
 
 const reqUpdateRate = {
-    rate_value: 3
-}
+    rate_value: 3,
+};
 
 module.exports = {
     reqAddRate,
     reqAddInvalidRate,
-    reqUpdateRate
-}
+    reqUpdateRate,
+};
