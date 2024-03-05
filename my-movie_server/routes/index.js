c4f9156f4e00d2a441a4c9de9a32258bd438ed3b
Maciej Mrozek <Maciej.h.mrozek@gmail.com>
2024-03-05 10:59:58 +0100

    Update index.js

--- my-movie_server/routes/index.js
+++ my-movie_server/routes/index.js
@@ -87,6 +87,7 @@ router.use((req, res, next) => {
 
 // FIXME: optimize pagination queries (LIMIT and OFFSET)
 // TODO: redesign error msgs and codes
+// test
 
 // TODO: add movie endpoints
 // movies
